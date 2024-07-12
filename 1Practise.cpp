e16718a406a36ad65e69470e0b0b821b209aa898
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-07-12 23:29:56 +0600

    commit-today

---
+++ 1Practise.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+        cout << a[i] << ' ';
+    }
+}
