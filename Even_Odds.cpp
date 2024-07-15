5c08aabcbe9dc1e49a0b58f8ffa04c602c1de395
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-07-15 23:26:59 +0600

    calculating-func

--- Even_Odds.cpp
+++ Even_Odds.cpp
@@ -1,12 +1,24 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     long long n, k;
+//     cin >> n >> k;
+//     n = (n + 1) / 2;
+//     if (k > n)
+//         cout << 2 * (k - n) << '\n';
+//     else
+//         cout << 2 * k - 1 << '\n';
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    long long n, k;
-    cin >> n >> k;
-    n = (n + 1) / 2;
-    if (k > n)
-        cout << 2 * (k - n) << '\n';
+    long long n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << n / 2 << '\n';
     else
-        cout << 2 * k - 1 << '\n';
+        cout << -((n / 2) + 1) << '\n';
 }
