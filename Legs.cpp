e658e65f3c3888af50b5e75ca1771a9821514c9a
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-07-27 22:47:20 +0600

    div3-A-1996

---
+++ Legs.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int ans = (n / 2) - (n / 4);
+        cout << ans << '\n';
+    }
+}
