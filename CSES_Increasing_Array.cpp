1708d3beb3375652263b1beede8bf6c72fb97fec
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-08-06 00:39:25 +0600

    cses4th

---
+++ CSES_Increasing_Array.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 9;
+int a[N];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+}
