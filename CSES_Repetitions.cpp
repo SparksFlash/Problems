fede888e448b6856f6bb2ac4c256fa074c631740
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-08-05 23:26:52 +0600

    Repetitions

--- CSES_Repetitions.cpp
+++ CSES_Repetitions.cpp
@@ -4,4 +4,17 @@ int main()
 {
     string s;
     cin >> s;
+    int cnt = 1, ans = 1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == s[i + 1])
+            cnt++;
+        else
+        {
+            ans = max(ans, cnt);
+            cnt = 1;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans << '\n';
 }
