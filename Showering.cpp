32b84c11781fb4912188f0095d992d3881a3dc12
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-08-18 21:19:34 +0600

    okay

--- Showering.cpp
+++ Showering.cpp
@@ -8,5 +8,22 @@ int main()
     while (t--)
     {
         cin >> n >> s >> m;
+        int p = 0;
+        string ans = "NO";
+        for (int i = 0; i < n; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            if (a - p >= s)
+            {
+                ans = "YES";
+            }
+            p = b;
+        }
+        if (m - p >= s)
+        {
+            ans = "YES";
+        }
+        cout << ans << endl;
     }
 }
