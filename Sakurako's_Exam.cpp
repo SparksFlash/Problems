4e5c50df46cb15ae2e697c1b97c8b1d5f755337d
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-09-03 23:32:44 +0600

    Div4-B

--- Sakurako's_Exam.cpp
+++ Sakurako's_Exam.cpp
@@ -3,4 +3,15 @@
 using namespace std;
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if ((a % 2 == 1 or a + b == 1) or (a == 0 and b % 2 == 1))
+            cout << "NO" << '\n';
+        else
+            cout << "YES" << '\n';
+    }
 }
