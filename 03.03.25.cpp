d3b8459d5a572c191016397507427630cf4a792b
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-03-04 00:33:22 +0600

    Polycrap and coins

---
+++ 03.03.25.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int c2 = floor((double)(n + 1) / 3);
+        int c1 = n - 2 * c2;
+        cout << c1 << " " << c2 << "\n";
+    }
+}
