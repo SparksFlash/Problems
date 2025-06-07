7760f2d9275beb0600a9226b3a4ab1b5d1a641e9
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-06-07 15:58:37 +0600

    solved

--- 5.1 Practice(STL)/Sets-STL.cpp
+++ 5.1 Practice(STL)/Sets-STL.cpp
@@ -4,4 +4,22 @@ int main()
 {
     int n;
     cin >> n;
+    set<int> s;
+
+    while (n--)
+    {
+        int y, x;
+        cin >> y >> x;
+        if (y == 1)
+            s.insert(x);
+        if (y == 2)
+            s.erase(x);
+        if (y == 3)
+        {
+            if (s.find(x) != s.end())
+                cout << "Yes" << '\n';
+            else
+                cout << "No" << '\n';
+        }
+    }
 }
