5e35f86209e28542241220378a5db57a25c439b5
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-01-24 23:35:30 +0600

    Spiral problem

--- CSES/Number Spiral.cpp
+++ CSES/Number Spiral.cpp
@@ -1,6 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int main(){
-    
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+    }
 }
