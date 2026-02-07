becd143e641e640c4a2b5ef2cb9b42a991f8acf8
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-02-07 13:34:54 +0600

    P. Minimize Number solved

--- Class 2/P. Minimize Number.cpp
+++ Class 2/P. Minimize Number.cpp
@@ -6,4 +6,25 @@ int main(){
     for(int i=0; i<n; i++){
         cin >> v[i];
     }
+
+    int operation = 0;
+    while(true){
+        bool is_all_even = true;
+        for(int i=0; i<n; i++){
+            if(v[i] % 2 == 1){
+               is_all_even = false;
+               break;
+            }
+        }
+
+        if(is_all_even){
+           for(int i=0; i<n; i++){
+              v[i] /= 2;
+            }
+           operation++;
+        }else{
+           break;
+        }
+    }
+    cout << operation << '\n';
 }
