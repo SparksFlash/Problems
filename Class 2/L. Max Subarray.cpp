6625ce5eb9fb81214cbe021c762448758100b961
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-02-07 15:28:33 +0600

    L. Max Subarray solved

--- Class 2/L. Max Subarray.cpp
+++ Class 2/L. Max Subarray.cpp
@@ -1,17 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void max_subarray(vector<int> &v){
+    int n = v.size();
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            int maximum = -1e6;
+            for(int k=i; k<=j; k++){
+                maximum = max(maximum,v[k]);
+            }
+             cout << maximum << ' ';
+        }
+    }
+    cout << '\n';
+}
+
 int main(){
     int t;
     cin >> t;
     while(t--){
         int n;
         cin >> n;
-        int a[n];
+        vector<int> v(n);
         for(int i = 0; i < n; i++){
-            cin >> a[i];
+            cin >> v[i];
         }
-
-        
+        max_subarray(v);
     }
 }
